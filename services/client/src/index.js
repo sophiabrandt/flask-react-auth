441953a925c40f0df435e67bcddf025f725e55b8
Sophia Brandt <git@s-br.net>
2019-12-26 10:14:14 +0100

    📦 NEW: add react form

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import AddUser from './components/AddUser'
 import UsersList from './components/UsersList'
 
 class App extends React.Component {
@@ -8,7 +9,11 @@ class App extends React.Component {
     super()
     this.state = {
       users: [],
+      username: '',
+      email: '',
     }
+    this.addUser = this.addUser.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidMount() {
@@ -26,6 +31,29 @@ class App extends React.Component {
       })
   }
 
+  addUser(event) {
+    event.preventDefault()
+    const data = {
+      username: this.state.username,
+      email: this.state.email,
+    }
+    axios
+      .post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+      .then(() => {
+        this.getUsers()
+        this.setState({ username: '', email: '' })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  handleChange(event) {
+    const obj = {}
+    obj[event.target.name] = event.target.value
+    this.setState(obj)
+  }
+
   render() {
     return (
       <section className="section">
@@ -34,6 +62,14 @@ class App extends React.Component {
             <div className="column is-one-third">
               <br />
               <h1 className="title is-1">Users</h1>
+              <hr />
+              <br />
+              <AddUser
+                addUser={this.addUser}
+                handleChange={this.handleChange}
+                email={this.state.email}
+                username={this.state.username}
+              />
               <hr />
               <br />
               <UsersList users={this.state.users} />
