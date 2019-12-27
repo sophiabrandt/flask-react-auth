e79c1e34a9fac3ef945b33d406b3882ad83d8316
Sophia Brandt <git@s-br.net>
2019-12-27 08:06:26 +0100

    📦 NEW: add flask-bcrypt and user password

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -5,12 +5,14 @@ import os
 
 from flask import Flask
 from flask_admin import Admin
+from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 
 # instantiate the extensions
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 cors = CORS()
 admin = Admin(template_mode="bootstrap3")
 
@@ -26,6 +28,7 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
+    bcrypt.init_app(app)
     cors.init_app(app)
     if os.getenv("FLASK_ENV") == "development":
         admin.init_app(app)
