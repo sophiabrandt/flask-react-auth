78ba28c42b9828e0edd12f0117a25be31735cd30
Sophia Brandt <git@s-br.net>
2019-12-29 14:50:18 +0100

    📦 NEW: add logout & status route

--- services/users/project/api/auth.py
+++ services/users/project/api/auth.py
@@ -67,3 +67,42 @@ def login_user():
     except Exception:
         response_object["message"] = "Try again."
         return jsonify(response_object), 500
+
+
+@auth_blueprint.route("/auth/logout", methods=["GET"])
+def logout_user():
+    # get auth token
+    auth_header = request.headers.get("Authorization")
+    response_object = {"status": "fail", "message": "Provide a valid auth token."}
+    if auth_header:
+        auth_token = auth_header.split(" ")[1]
+        resp = User.decode_auth_token(auth_token)
+        if not isinstance(resp, str):
+            response_object["status"] = "success"
+            response_object["message"] = "Successfully logged out."
+            return jsonify(response_object), 200
+        else:
+            response_object["message"] = resp
+            return jsonify(response_object), 401
+    else:
+        return jsonify(response_object), 403
+
+
+@auth_blueprint.route("/auth/status", methods=["GET"])
+def get_user_status():
+    # get auth token
+    auth_header = request.headers.get("Authorization")
+    response_object = {"status": "fail", "message": "Provide a valid auth token."}
+    if auth_header:
+        auth_token = auth_header.split(" ")[1]
+        resp = User.decode_auth_token(auth_token)
+        if not isinstance(resp, str):
+            user = User.query.filter_by(id=resp).first()
+            response_object["status"] = "success"
+            response_object["message"] = "Success."
+            response_object["data"] = user.to_json()
+            return jsonify(response_object), 200
+        response_object["message"] = resp
+        return jsonify(response_object), 401
+    else:
+        return jsonify(response_object), 401
--- services/users/project/tests/test_auth.py
+++ services/users/project/tests/test_auth.py
@@ -1,5 +1,7 @@
 import json
 
+from flask import current_app
+
 from project.tests.utils import add_user
 
 
@@ -94,7 +96,7 @@ def test_user_registration_invalid_json_keys_no_email(test_app):
     assert resp.content_type == "application/json"
 
 
-def test_user_registration_invalid_json_keys_no_password(test_app, test_database):
+def test_user_registration_invalid_json_keys_no_password(test_app):
     client = test_app.test_client()
     resp = client.post(
         "/auth/register",
@@ -124,7 +126,7 @@ def test_registered_user_login(test_app):
     assert resp.content_type == "application/json"
 
 
-def test_not_registered_user_login(test_app, test_database):
+def test_not_registered_user_login(test_app):
     client = test_app.test_client()
     resp = client.post(
         "/auth/login",
@@ -134,5 +136,105 @@ def test_not_registered_user_login(test_app, test_database):
     data = json.loads(resp.data.decode())
     assert resp.status_code == 404
     assert "User does not exist." in data["message"]
+    assert "fail" in data["status"]
+    assert resp.content_type == "application/json"
+
+
+def test_valid_logout(test_app):
+    add_user("test4", "test4@test.com", "test")
+    client = test_app.test_client()
+    # user login
+    resp_login = client.post(
+        "/auth/login",
+        data=json.dumps({"email": "test4@test.com", "password": "test"}),
+        content_type="application/json",
+    )
+    # valid token logout
+    token = json.loads(resp_login.data.decode())["auth_token"]
+    resp = client.get(
+        "/auth/logout",
+        headers={"Authorization": f"Bearer {token}"},
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert "Successfully logged out." in data["message"]
+    assert "success" in data["status"]
+    assert resp.content_type == "application/json"
+
+
+def test_invalid_logout_expired_token(test_app):
+    add_user("test5", "test5@test.com", "test")
+    current_app.config["TOKEN_EXPIRATION_SECONDS"] = -1
+    client = test_app.test_client()
+    # user login
+    resp_login = client.post(
+        "/auth/login",
+        data=json.dumps({"email": "test5@test.com", "password": "test"}),
+        content_type="application/json",
+    )
+    # invalid token logout
+    token = json.loads(resp_login.data.decode())["auth_token"]
+    resp = client.get(
+        "/auth/logout",
+        headers={"Authorization": f"Bearer {token}"},
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 401
+    assert "Signature expired. Please log in again." in data["message"]
+    assert "fail" in data["status"]
+    assert resp.content_type == "application/json"
+    current_app.config["TOKEN_EXPIRATION_SECONDS"] = 3
+
+
+def test_invalid_logout(test_app):
+    client = test_app.test_client()
+    resp = client.get(
+        "/auth/logout",
+        headers={"Authorization": "Bearer invalid"},
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 401
+    assert "Invalid token. Please log in again." in data["message"]
+    assert "fail" in data["status"]
+    assert resp.content_type == "application/json"
+
+
+def test_user_status(test_app):
+    add_user("test6", "test6@test.com", "test")
+    client = test_app.test_client()
+    resp_login = client.post(
+        "/auth/login",
+        data=json.dumps({"email": "test6@test.com", "password": "test"}),
+        content_type="application/json",
+    )
+    token = json.loads(resp_login.data.decode())["auth_token"]
+    resp = client.get(
+        "/auth/status",
+        headers={"Authorization": f"Bearer {token}"},
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert "success" in data["status"]
+    assert data["data"] is not None
+    assert data["data"]["username"] == "test6"
+    assert data["data"]["email"] == "test6@test.com"
+    assert data["data"]["active"] is True
+    assert resp.content_type == "application/json"
+
+
+def test_invalid_status(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.get(
+        "/auth/status",
+        headers={"Authorization": "Bearer invalid"},
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 401
+    assert "Invalid token. Please log in again." in data["message"]
     assert "fail" in data["status"]
     assert resp.content_type == "application/json"
