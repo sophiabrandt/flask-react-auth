c9b662b615f40826d3c0e220a04f96627ec5a05d
Sophia Brandt <git@s-br.net>
2020-01-01 14:02:33 +0100

    📦 NEW: add yup and formik

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -1,25 +1,22 @@
 import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom'
 import axios from 'axios'
+import { Route, Switch } from 'react-router-dom'
 
-import About from './components/About'
-import AddUser from './components/AddUser'
-import LoginForm from './components/LoginForm'
-import NavBar from './components/NavBar'
-import RegisterForm from './components/RegisterForm'
 import UsersList from './components/UsersList'
+import AddUser from './components/AddUser'
+import About from './components/About'
+import NavBar from './components/NavBar'
+import LoginForm from './components/LoginForm'
+import RegisterForm from './components/RegisterForm'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       users: [],
-      username: '',
-      email: '',
       title: 'Testdriven App',
     }
     this.addUser = this.addUser.bind(this)
-    this.handleChange = this.handleChange.bind(this)
   }
   componentDidMount() {
     this.getUsers()
@@ -34,12 +31,7 @@ class App extends Component {
         console.log(err)
       })
   }
-  addUser(event) {
-    event.preventDefault()
-    const data = {
-      username: this.state.username,
-      email: this.state.email,
-    }
+  addUser(data) {
     axios
       .post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
       .then(() => {
@@ -49,11 +41,6 @@ class App extends Component {
       .catch(err => {
         console.log(err)
       })
-  }
-  handleChange(event) {
-    const obj = {}
-    obj[event.target.name] = event.target.value
-    this.setState(obj)
   }
   render() {
     return (
@@ -73,12 +60,8 @@ class App extends Component {
                         <h1 className="title is-1">Users</h1>
                         <hr />
                         <br />
-                        <AddUser
-                          username={this.state.username}
-                          email={this.state.email}
-                          addUser={this.addUser}
-                          handleChange={this.handleChange}
-                        />
+                        {/* updated */}
+                        <AddUser addUser={this.addUser} />
                         <br />
                         <br />
                         <UsersList users={this.state.users} />
--- services/client/src/components/UsersList.jsx
+++ services/client/src/components/UsersList.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const UsersList = props => {
+  // console.log(props.users)
   return (
     <div>
       {props.users.map(user => {
