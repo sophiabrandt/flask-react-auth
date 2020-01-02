99b0f27102a36453d95789b99696b7500f68317b
Sophia Brandt <git@s-br.net>
2020-01-02 08:54:14 +0100

    📦 NEW: React authentication

--- services/client/src/components/__tests__/LoginForm.test.jsx
+++ services/client/src/components/__tests__/LoginForm.test.jsx
@@ -6,12 +6,33 @@ import LoginForm from '../LoginForm'
 
 afterEach(cleanup)
 
+const props = {
+  handleLoginFormSubmit: () => {
+    return true
+  },
+}
+
 it('renders properly', () => {
-  const { getByText } = render(<LoginForm />)
+  const { getByText } = render(<LoginForm {...props} />)
   expect(getByText('Log In')).toHaveClass('title')
 })
 
+it('renders with default props', () => {
+  const { getByLabelText, getByText } = render(<LoginForm {...props} />)
+
+  const emailInput = getByLabelText('Email')
+  expect(emailInput).toHaveAttribute('type', 'email')
+  expect(emailInput).not.toHaveValue()
+
+  const passwordInput = getByLabelText('Password')
+  expect(passwordInput).toHaveAttribute('type', 'password')
+  expect(passwordInput).not.toHaveValue()
+
+  const buttonInput = getByText('Submit')
+  expect(buttonInput).toHaveValue('Submit')
+})
+
 it('renders', () => {
-  const { asFragment } = render(<LoginForm />)
+  const { asFragment } = render(<LoginForm {...props} />)
   expect(asFragment()).toMatchSnapshot()
 })
--- services/client/src/components/__tests__/RegisterForm.test.jsx
+++ services/client/src/components/__tests__/RegisterForm.test.jsx
@@ -6,12 +6,37 @@ import RegisterForm from '../RegisterForm'
 
 afterEach(cleanup)
 
+const props = {
+  handleRegisterFormSubmit: () => {
+    return true
+  },
+}
+
 it('renders properly', () => {
-  const { getByText } = render(<RegisterForm />)
+  const { getByText } = render(<RegisterForm {...props} />)
   expect(getByText('Register')).toHaveClass('title')
 })
 
+it('renders with default props', () => {
+  const { getByLabelText, getByText } = render(<RegisterForm {...props} />)
+
+  const usernameInput = getByLabelText('Username')
+  expect(usernameInput).toHaveAttribute('type', 'text')
+  expect(usernameInput).not.toHaveValue()
+
+  const emailInput = getByLabelText('Email')
+  expect(emailInput).toHaveAttribute('type', 'email')
+  expect(emailInput).not.toHaveValue()
+
+  const passwordInput = getByLabelText('Password')
+  expect(passwordInput).toHaveAttribute('type', 'password')
+  expect(passwordInput).not.toHaveValue()
+
+  const buttonInput = getByText('Submit')
+  expect(buttonInput).toHaveValue('Submit')
+})
+
 it('renders', () => {
-  const { asFragment } = render(<RegisterForm />)
+  const { asFragment } = render(<RegisterForm {...props} />)
   expect(asFragment()).toMatchSnapshot()
 })
